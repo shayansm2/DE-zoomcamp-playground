a512ebd8cc2dcc53890b766963a066fdd58176d2
shayan <shayansm09@gmail.com>
2023-02-04 21:05:25 +0330

    complete the etl pipeline

--- project/data_ingestion/etl.py
+++ project/data_ingestion/etl.py
@@ -1,18 +1,33 @@
+import argparse
+from typing import Iterator
 import pandas as pd
 from prefect import flow, task
 
-from utils.os_helpers import download_if_not_exists, unzip
+from project.utils.os_helpers import download_if_not_exists, unzip
+from project.utils.loggers import print_logger
+
+from project.database.PostgresDB import PostgresDB
 
 
 @flow(name='ingest-github-archive')
+@print_logger
 def etl(year: int, month: int, day: int, hour: int):
-    data = extract(year, month, day, hour)
-    data = transform(data)
-    load(data)
+    file_path = extract(year, month, day, hour)
+
+    chunk_size = 5000
+    data_iter = pd.read_json(file_path, lines=True, chunksize=chunk_size)
+
+    try:
+        data = next(data_iter)
+        data = transform(data)
+        load(data)
+    except StopIteration:
+        return
 
 
 @task(retries=1, cache_result_in_memory=True, log_prints=True)
-def extract(year: int, month: int, day: int, hour: int) -> pd.DataFrame:
+@print_logger
+def extract(year: int, month: int, day: int, hour: int) -> str:
     download_path = get_download_path(year, month, day, hour)
 
     print('start downloading data')
@@ -23,21 +38,19 @@ def extract(year: int, month: int, day: int, hour: int) -> pd.DataFrame:
     file_path = unzip(file_path)
     print('unzipping finished')
 
-    print('start converting to dataframe')
-    df = pd.read_json(file_path, lines=True)
-    print('dataframe created')
-
-    return df
+    return file_path
 
 
-@task()
+@task(log_prints=True)
+@print_logger
 def transform(data: pd.DataFrame) -> pd.DataFrame:
-    return data
+    return data[['id', 'type', 'public', 'created_at']]
 
 
-@task(retries=3)
+@task(retries=1, log_prints=True)
+@print_logger
 def load(data: pd.DataFrame):
-    print(data.head())
+    PostgresDB().insert(data, 'github_archives')
 
 
 def get_download_path(year: int, month: int, day: int, hour: int):
@@ -52,3 +65,23 @@ def get_str_from_int(number: int):
         return '0' + str(number)
 
     return str(number)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Ingest CSV data to Postgres')
+
+    parser.add_argument('--year')
+    parser.add_argument('--month')
+    parser.add_argument('--day')
+    parser.add_argument('--hour')
+
+    args = parser.parse_args()
+
+    year = args.year
+    month = args.month
+    day = args.day
+    hour = args.hour
+
+    assert year is not None
+
+    etl(year, month, day, hour)
