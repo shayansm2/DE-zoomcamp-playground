5577b1074d86dbecae31fcdea1ca0335f199ef38
shayan <shayansm09@gmail.com>
2024-04-12 16:39:04 +0330

    add username and password to elastic Configs

--- kafka-playground/src/main/java/org/example/Configs.java
+++ kafka-playground/src/main/java/org/example/Configs.java
@@ -4,5 +4,7 @@ public class Configs {
     public static final String KAFKA_BOOTSTRAP_SERVER = "localhost:9092";
     public static final String KAFKA_TOPIC_NAME = "github_events";
     public static final String ELASTICSEARCH_SERVER = "localhost:9200";
-    public static final String ELASTICSEARCH_INDEX_NAME = "github_events";
+    public static final String ELASTICSEARCH_USERNAME = "username";
+    public static final String ELASTICSEARCH_PASSWORD = "password";
+    public static final String ELASTICSEARCH_INDEX_EVENTS = "github_events_v2";
 }
