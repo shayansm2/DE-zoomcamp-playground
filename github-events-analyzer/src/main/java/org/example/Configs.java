1607c72c1726204c82a507967c56d94814cf8ac3
shayan <shayansm09@gmail.com>
2024-04-16 00:35:59 +0330

    revert

--- github-events-analyzer/src/main/java/org/example/Configs.java
+++ github-events-analyzer/src/main/java/org/example/Configs.java
@@ -3,10 +3,10 @@ package org.example;
 public class Configs {
     public static final String KAFKA_BOOTSTRAP_SERVER = "localhost:9092";
     public static final String KAFKA_TOPIC_NAME = "github_events_v2";
-    public static final String ELASTICSEARCH_SERVER_CLOUD = "https://de-zoomcamp-project2-elasticsearch.darkube.app";
+    public static final String ELASTICSEARCH_SERVER_CLOUD = "localhost:9200";
     public static final String ELASTICSEARCH_SERVER_LOCAL = "localhost:9200";
-    public static final String ELASTICSEARCH_USERNAME = "elastic";
-    public static final String ELASTICSEARCH_PASSWORD = "qpaS680E8qmaR092p72T0wUoKtjCQSYB";
+    public static final String ELASTICSEARCH_USERNAME = "username";
+    public static final String ELASTICSEARCH_PASSWORD = "password";
     public static final String ELASTICSEARCH_INDEX_EVENTS = "github_events";
 
     public enum Modes {
